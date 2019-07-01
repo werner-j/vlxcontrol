0c74b20656b6061cc6bdcb62b2c1bd5ccb7d282f
werner-j <git@janjic.net>
2019-07-01 20:40:00 +0200

    Catch exception.

--- vlxcontrol.py
+++ vlxcontrol.py
@@ -78,12 +78,16 @@ async def get_position(request):
 
 @routes.get('/devices')
 async def get_devices(request):
-    data = { 'result':'ok', 'devices':[] }
-    for node in pyvlx.nodes:
-        nodetype = str(type(node).__name__)
-        data['devices'].append({'id':node.node_id,'name':node.name,'type':nodetype})
-        print(node)
-    return web.json_response(data)
+    try:
+        data = { 'result':'ok', 'devices':[] }
+        for node in pyvlx.nodes:
+            nodetype = str(type(node).__name__)
+            data['devices'].append({'id':node.node_id,'name':node.name,'type':nodetype})
+            print(node)
+        return web.json_response(data)
+    except:
+        response = { 'result':'fail' }
+        return web.json_response(response)
 
 if __name__ == '__main__':
     LOOP = asyncio.get_event_loop()
